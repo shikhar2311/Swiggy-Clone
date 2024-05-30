1dcf01cc0c6448ff7060d8957a3223f472d1e03c
Shikhar Rajput <shikharrajput@192.168.1.9>
2024-05-30 18:41:58 +0530

    abc

--- src/Components/body.js
+++ src/Components/body.js
@@ -1,20 +1,17 @@
 // import { useState } from "react";
-import { restaurantList } from "../constants"; 
+import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 
-
 function filterData(searchText, restaurants) {
-  const filterData = restaurants.filter((restaurant) => 
+  const filterData = restaurants.filter((restaurant) =>
     restaurant.data.name.includes(searchText)
   );
   return filterData;
-
 }
 
-const Body = () =>{
-
-  const [restaurants, setRestaurants] = useState(restaurantList);
+const Body = () => {
+  const [restaurants, setRestaurants] = useState([]);
   const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
@@ -36,38 +33,37 @@ const Body = () =>{
 
   console.log("render");
 
-    return (
-      <>
+  return (
+    <>
       <div className="search- container">
-        <input 
-        type="text" 
-        className="search-input" 
-        placeholder="Search" 
-        value={searchText}
-        onChange={(e) => {
-          setSearchInput(e.target.value);
-        }}
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
         />
-        <button 
-        className="search-btn"
-        onClick={() =>{
-          const data = filterData(searchText, restaurants);
-          setRestaurants(data);
-        }}
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, restaurants);
+            setRestaurants(data);
+          }}
         >
           Search
         </button>
       </div>
       <div className="restaurant-list">
-      {
-        restaurants.map((restaurant) =>{
-          return (<RestaurantCard {...restaurant.data} key={restaurant.data.id}/>
-        );
-        })
-      }
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
       </div>
-      </>
-    )
-  }
+    </>
+  );
+};
 
-  export default Body;
+export default Body;
